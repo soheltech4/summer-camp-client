74cdef475b147a4956a7cfdea0795e7dcba652ff
sohelranagtc <sohelranagtc@gmail.com>
2023-06-09 17:47:40 +0600

    added Card

--- src/Pages/Home/TopClasses/TopClasses.jsx
+++ src/Pages/Home/TopClasses/TopClasses.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import UseClasses from '../../../Components/Classes/UseClasses';
+import ClassesCard from './ClassesCard';
 
 const TopClasses = () => {
     const [classes] = UseClasses()
     return (
-        <div>
-            
+        <div className='grid md:grid-cols-3 gap-5 justify-center'>
+            {
+                classes.map(CData => 
+                <ClassesCard
+                    key={CData._id} 
+                    CData={CData}
+                ></ClassesCard>)
+            }
         </div>
     );
 };
