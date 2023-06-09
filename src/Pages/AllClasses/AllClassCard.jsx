0454e1448fa1ac565712522401330c4700a1998c
sohelranagtc <sohelranagtc@gmail.com>
2023-06-10 02:22:56 +0600

    add data

--- src/Pages/AllClasses/AllClassCard.jsx
+++ src/Pages/AllClasses/AllClassCard.jsx
@@ -1,18 +1,73 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../Provider/AuthProvider';
+import Swal from 'sweetalert2';
+import {useLocation, useNavigate } from 'react-router-dom';
+import { data } from 'autoprefixer';
 
 const AllClassCard = ({ cls }) => {
-    const { availableSeats, courseName, image, instructorName, price, _id, totalSeats } = cls
+    const { availableSeats, courseName, image, instructorName, price, _id, totalSeats } = cls;
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const isSeatsLimited = availableSeats === 0;
+
+    const cardClass = `card w-96 hover:bg-blue-100 shadow-xl ${isSeatsLimited ? 'bg-red-500' : ''}`;
+    const buttonDisabled = isSeatsLimited;
+
+    const handleSelect = id => {
+        console.log(id)
+        if (user && user?.email ) {
+            const selectClass = {ClassId : id, availableSeats, courseName, image, instructorName, price, totalSeats, email: user?.email  }
+            fetch('http://localhost:5000/select', {
+                method : "POST",
+                headers : {
+                    'content-type' : 'application/json'
+                },
+                body : JSON.stringify(selectClass)
+            })
+            .then(res => res.json())
+            .then( data => {
+                if(data.insertedId){
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: `Your ${courseName} Class Added Successfully`,
+                        showConfirmButton: false,
+                        timer: 1500
+                      })
+                }
+            })
+        }
+        else{
+            Swal.fire({
+                title: 'Please Login before Select',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login Now!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login', { state: { from: location }})
+                }
+            })
+        }
+    }
+
     return (
-        <div className="card w-96 hover:bg-blue-100 shadow-xl">
-            <figure><img src={image} className='w-full h-48' alt="Shoes" /></figure>
+        <div className={cardClass}>
+            <figure>
+                <img src={image} className="w-full h-48" alt="Shoes" />
+            </figure>
             <div className="card-body">
                 <h2 className="card-title uppercase">{courseName}</h2>
-                <p className='text-xl font-semibold'>Instructor : {instructorName}</p>
-                <p className='text-xl'>Total Seats: {totalSeats}</p>
-                <p className='text-xl'>Available Seats: {availableSeats}</p>
-                <p className='text-xl'>Price: ${price}</p>
+                <p className="text-xl font-semibold">Instructor: {instructorName}</p>
+                <p className="text-xl">Total Seats: {totalSeats}</p>
+                <p className="text-xl">Available Seats: {availableSeats}</p>
+                <p className="text-xl">Price: ${price}</p>
                 <div className="card-actions justify-center">
-                    <button className="btn btn-primary w-full">Select</button>
+                    <button onClick={() => handleSelect(_id)} className="btn btn-primary w-full" disabled={buttonDisabled}>Select</button>
                 </div>
             </div>
         </div>
--- src/Pages/Home/TopClasses/ClassesCard.jsx
+++ src/Pages/Home/TopClasses/ClassesCard.jsx
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Swal from 'sweetalert2';
+import {useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../../Provider/AuthProvider';
 
 const ClassesCard = ({ CData }) => {
-    console.log(CData)
-    const {availableSeats, courseName, image, instructorName, price, _id, totalSeats } = CData
+    const { availableSeats, courseName, image, instructorName, price, _id, totalSeats } = CData;
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate()
+    const location = useLocation()
+
+    const isSeatsLimited = availableSeats === 0;
+
+    const cardClass = `card w-96 hover:bg-blue-100 shadow-xl ${isSeatsLimited ? 'bg-red-500' : ''}`;
+    const buttonDisabled = isSeatsLimited;
+
+    const handleSelect = id => {
+        console.log(id)
+        if (!user) {
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able without Login",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login Now!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login', { state: { from: location }})
+                }
+            })
+        }
+    }
+
     return (
-        <div className="card w-96 hover:bg-blue-100 shadow-xl">
-            <figure><img src={image} className='w-full h-48' alt="Shoes" /></figure>
+        <div className={cardClass}>
+            <figure>
+                <img src={image} className="w-full h-48" alt="Shoes" />
+            </figure>
             <div className="card-body">
                 <h2 className="card-title uppercase">{courseName}</h2>
-                <p className='text-xl font-semibold'>Instructor : {instructorName}</p>
-                <p className='text-xl'>Total Seats: {totalSeats}</p>
-                <p className='text-xl'>Available Seats: {availableSeats}</p>
-                <p className='text-xl'>Price: ${price}</p>
+                <p className="text-xl font-semibold">Instructor: {instructorName}</p>
+                <p className="text-xl">Total Seats: {totalSeats}</p>
+                <p className="text-xl">Available Seats: {availableSeats}</p>
+                <p className="text-xl">Price: ${price}</p>
                 <div className="card-actions justify-center">
-                    <button className="btn btn-primary w-full">Select</button>
+                    <button onClick={() => handleSelect(_id)} className="btn btn-primary w-full" disabled={buttonDisabled}>Select</button>
                 </div>
             </div>
         </div>
